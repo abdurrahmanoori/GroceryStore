084e96e391e7f160b296d6c85a3565b35e240641
Abudurrahman <Abdurrahmannoori10@gmail.com>
2022-09-05 22:30:29 +0430

    Added Item Index related stuff

--- GroceryStore/Controllers/ItemController.cs
+++ GroceryStore/Controllers/ItemController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GroceryStore.Data;
+using GroceryStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using GroceryStore.ViewModels;
 
 namespace GroceryStore.Controllers
 {
@@ -12,17 +14,48 @@ namespace GroceryStore.Controllers
         private readonly AppDbContext _db;
         public ItemController(AppDbContext db)
         {
-            _db = db; 
+            _db = db;
         }
         public IActionResult Index()
         {
-            return View(_db.Item.ToList());
+            DAL d = new DAL();
+            d.connect();
+            d.cmd.CommandText = "SELECT dbo.itemStoreKeeper.Id, dbo.itemStoreKeeper.Name,dbo.Category.Name AS Category, dbo.Item.Price,dbo.item.Discount,dbo.itemStoreKeeper.Photo FROM((dbo.itemStoreKeeper INNER JOIN dbo.Category ON dbo.itemStoreKeeper.CategoryId = dbo.Category.Id) INNER JOIN dbo.Item ON dbo.Item.Id = dbo.itemStoreKeeper.Id)";
+            d.cmd.Connection = d.con;
+            d.dr = d.cmd.ExecuteReader();
+            List<ItemIndexViewModel> itemList = new List<ItemIndexViewModel>();
+            while (d.dr.Read())
+            {
+                ItemIndexViewModel itemIndexViewModel = new ItemIndexViewModel();
+                itemIndexViewModel.Id = Convert.ToInt32(d.dr["Id"]);
+                itemIndexViewModel.Name = d.dr["Name"].ToString();
+                itemIndexViewModel.Category = d.dr["Category"].ToString();
+                itemIndexViewModel.Price = Convert.ToInt32(d.dr["Price"]);
+                if (d.dr["Discount"] is int)
+                    itemIndexViewModel.Discount = Convert.ToInt32(d.dr["Discount"]);
+                itemList.Add(itemIndexViewModel);
+            }
+            d.disconnect();
+            return View(itemList);
         }
+        //GET Create
         public IActionResult Create()
         {
+            ViewBag.ItemList = _db.itemStoreKeeper.ToList();
+            ViewBag.CategoryList = _db.Category.ToList();
             return View();
         }
+        //POST Create
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _db.Item.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit()
+        {
 
-
+        }
     }
 }
